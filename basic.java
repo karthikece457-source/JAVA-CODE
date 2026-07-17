bf444888a687e63f48fb16725c1fee90b9192a64
karthik k <karthikece457@gmail.com>
2026-07-17 15:46:48 +0530

    Basic.java

---
+++ basic.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args)
+    {
+        String str1="karthik";
+        String str2="ece";
+        double a=7.7;
+        System.out.println("name="+str1);
+        System.out.println("class="+str2);
+        System.out.println("cgpa="+a);
+
+    }
+}
