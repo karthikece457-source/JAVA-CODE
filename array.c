dca775aba51e39e0c78a7e923d0d89cfcce0f3af
karthik k <karthikece457@gmail.com>
2026-07-20 15:10:40 +0530

    Array.c

---
+++ array.c
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+        int[]marks={498,457,467,456,378};
+        String[]name={"dharun","karthik","vishnu","dharanish","venugopal"};
+        for(int i=0;i<marks.length;i++)
+        {
+            System.out.println(name[i]+ "="+marks[i]);
+        }
+    }
+
+}
