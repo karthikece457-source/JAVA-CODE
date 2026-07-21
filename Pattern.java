db668b7bc03fce2d53f211689ccee05617cc9512
karthik k <karthikece457@gmail.com>
2026-07-21 14:41:08 +0530

    Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,15 @@
+public class patt {
+    public static void main(String[]args) {
+        for(int i=0;i<4;i++){
+            for(int j=0;j<i;j++)
+            {
+                System.out.print("  " );
+            }
+            for(int j=0;j<4-i;j++)
+        {
+            System.out.print(j+1);
+        }
+        System.out.println();
+    }
+}
+}
